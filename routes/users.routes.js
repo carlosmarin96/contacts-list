a27f585e8ff80f7c299f3a90f5539e0e1cd09e31
carlosmarin96 <caa7x21@gmail.com>
2021-12-17 01:18:49 -0700

    Validate role with database

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,6 +3,7 @@ const { check } = require('express-validator');
 
 const { postUser, putUser, deleteUser } = require('../controllers/users.controllers');
 const { validateFields } = require('../middlewares/validate-fields');
+const Role = require('../models/role');
 
 const router = Router();
 
@@ -11,7 +12,13 @@ router.post('/', [
     check('lastName', 'Lastname is required').not().isEmpty(),
     check('email', 'Email is not valid').isEmail(),
     check('password', 'Password must be 6 characters or more').isLength({ min: 6 }),
-    check('role', 'Role is not valid').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // check('role', 'Role is not valid').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('role').custom(async (role = '') => {
+        const roleExist = await Role.findOne({ role });
+        if (!roleExist) {
+            throw new Error(`Role ${role} is not valid`);
+        }
+    }),
     validateFields
 ], postUser);
 
