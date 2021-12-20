498061b1f392f8e2233f201b4eabe879e23db4f5
carlosmarin96 <caa7x21@gmail.com>
2021-12-19 23:16:35 -0700

    Take out pass and v from json

--- models/user.js
+++ models/user.js
@@ -40,5 +40,9 @@ const UserSchema = Schema({
     }
 });
 
+UserSchema.methods.toJSON = function () {
+    const { __v, password, ...user } = this.toObject();
+    return user;
+}
 
 module.exports = model('user', UserSchema);
